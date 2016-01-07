bed0eb9d8d455d5f22f790ead3f04aa0615cb008
Luis Dalves <luis@flowics.com>
2016-01-07 14:39:43 -0300

    Commit #51

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express"),
     mongoose = require("mongoose"),
     app = express();
 
-mongoose.connect("mongodb://localhost/test", function (err) {
+mongoose.connect("mongodb://172.16.42.1/test", function (err) {
   if (!err) {
     console.log("Connected to MongoDB");
   } else {
